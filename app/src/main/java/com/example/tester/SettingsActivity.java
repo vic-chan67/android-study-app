4a55ff5047f2e15eef2a5b494c0af616aaed34a8
victoriachan <victoriachan67@gmail.com>
2024-03-22 19:00:29 +0000

    Create a settings page

---
+++ app/src/main/java/com/example/tester/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.example.tester;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.tester.databinding.ActivityMainBinding;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
